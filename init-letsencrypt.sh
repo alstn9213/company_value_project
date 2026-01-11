109aa42e96119381d8af5933366d9e98d74ae83c
alstn9213 <alstn9213@naver.com>
2026-01-11 13:06:29 +0900

    feat:20260111 더미 인증서 자동화 스크립트 작성

---
+++ init-letsencrypt.sh
@@ -0,0 +1,66 @@
+domains=(valuepick.p-e.kr) # 도메인 설정 (여러 개면 띄어쓰기로 구분)
+rsa_key_size=4096
+data_path="./data/certbot"
+email="alstn9213@naver.com"
+staging=0 # 0이면 정식 발급, 1이면 테스트 발급 (테스트 실패 방지)
+
+
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+  echo "### SSL 필수 설정 파일 다운로드 중 ..."
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
+fi
+
+echo "### 더미(Dummy) 인증서 생성 중 ($domains) ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
+
+
+echo "### Nginx 시작 중 ..."
+docker compose -f docker-compose.prod.yml up --force-recreate -d nginx
+echo
+
+echo "### 더미 인증서 삭제 중 ..."
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
+
+
+echo "### Let's Encrypt 진짜 인증서 요청 중 ($domains) ..."
+# 도메인 인자 구성
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# 이메일 설정
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="-m $email" ;;
+esac
+
+# 스테이징 모드 설정
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
+
+echo "### Nginx 재설정 (인증서 적용) ..."
+docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
